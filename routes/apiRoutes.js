96926b08aede95d5fddfe15b58d2d434cda7697f
Ian Clark <ianclark226@gmail.com>
2020-02-23 15:00:47 -0500

    2/23

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,33 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/burgers', () => {
+router.get('/burgers', async (_, res) => {
+const data = await db.Burger.findAll();
 
+res.json(data);
+});
+
+router.post('/burger/new', async (_, res) => {
+    const {name} = req.body;
+
+    await db.Burger.creat({
+
+        isDevoured: true
+    });
+
+    res.status(200).end();
+});
+
+router.put('/burger/:id/devour', async (req, res) => {
+    const { id } = req.param;
+
+    await db.Burger.update({
+        isDevoured: true
+    }, {
+        where: {
+            id
+        }
+    })
 });
 
 module.exports = router;
