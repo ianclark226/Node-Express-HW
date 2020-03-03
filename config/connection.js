c6312e3444fdab019410956e3b2f41b576126612
Ian Clark <ianclark226@gmail.com>
2020-03-03 11:21:12 -0500

    3/3

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 var Sequelize = require("sequelize");
-var connection;
+// var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'yourRootPassword',
-        database: 'burgers_db'
-    });
-}
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'yourRootPassword',
+//         database: 'burgers_db'
+//     });
+// }
 
-connection.connect();
+// connection.connect();
 
 var sequelize = new Sequelize("burgers_db", "root", "yourRootPassword", {
     host: "localhost",
